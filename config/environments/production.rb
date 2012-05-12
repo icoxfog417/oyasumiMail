95764f324470e5b28bcf1906fc811c1cbfba9abd
Takahiro Kubo <icoxfog417@yahoo.co.jp>
2012-05-12 17:42:58 +0900

    modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ OyasumiMail::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-    conﬁg.action_mailer.default_url_options ={ :host => 'xxxx.herokuapp.com' }
+    config.action_mailer.default_url_options ={ :host => 'xxxx.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
