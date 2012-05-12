250fd5495eb61fff1534b49f9c877c4cfa75bc04
Takahiro Kubo <icoxfog417@yahoo.co.jp>
2012-05-12 22:38:03 +0900

    add user restriction

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,8 +4,9 @@ class MessagesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @messages = Message.all
-
+    #@messages = Message.all
+    @messages = Message.where("user_id = ?" , current_user.id).all
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @messages }
@@ -15,8 +16,8 @@ class MessagesController < ApplicationController
   # GET /messages/1
   # GET /messages/1.json
   def show
-    @message = Message.find(params[:id])
-
+    @message = Message.where("user_id = ?" , current_user.id).find(params[:id])
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @message }
@@ -36,13 +37,15 @@ class MessagesController < ApplicationController
 
   # GET /messages/1/edit
   def edit
-    @message = Message.find(params[:id])
+    @message = Message.where("user_id = ?" , current_user.id).find(params[:id])
+    
   end
 
   # POST /messages
   # POST /messages.json
   def create
     @message = Message.new(params[:message])
+    @message.user_id = current_user.id
 
     respond_to do |format|
       if @message.save
@@ -58,7 +61,7 @@ class MessagesController < ApplicationController
   # PUT /messages/1
   # PUT /messages/1.json
   def update
-    @message = Message.find(params[:id])
+    @message = Message.where("user_id = ?" , current_user.id).find(params[:id])
 
     respond_to do |format|
       if @message.update_attributes(params[:message])
@@ -74,7 +77,7 @@ class MessagesController < ApplicationController
   # DELETE /messages/1
   # DELETE /messages/1.json
   def destroy
-    @message = Message.find(params[:id])
+    @message = Message.where("user_id = ?" , current_user.id).find(params[:id])
     @message.destroy
 
     respond_to do |format|
